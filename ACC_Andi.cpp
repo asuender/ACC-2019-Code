388efde164a62a9e894376de5a315f327b480f9e
ProgrammeYourself <andi.suender@outlook.de>
2019-12-22 11:02:08 +0100

    NEW VERSION

--- ACC_Andi.cpp
+++ ACC_Andi.cpp
@@ -4,73 +4,67 @@
 #include <iostream>
 #include <algorithm>
 #include <time.h>
-#include <thread>
 #include <future>
+#include <numeric>
 
-//some global variables
-unsigned long j;
-unsigned long i;
 
 template<typename T>
-T min(std::vector<T> v) {
-    T min = (v.size() == 1) ? v.at(0) : *std::min_element(v.begin(), v.end());
-    return min;
+long long min(std::vector<T> v) {
+    long long min_e = (v.size() == 1) ? v.at(0) : *std::min_element(v.begin(), v.end());
+    return min_e;
 }
 
-int S(int n=0) {
-    return 290797;
-}
 
 template<typename N>
-N S(N n) {
-    return (S(n-1)*S(n-1)) % 50515093;
-}
+long long S(N n) {
+    if (n==0)
+        return 290797;
+    long long s = S(n-1);
+    return (s*s) % 50515093;
+}  
 
 template<typename I, typename J>
-unsigned long A(I i, J j) {
-    std::vector<unsigned long> list = {};
-    for (auto x=i; x<=j; x++) list.push_back(S(x));
+long long A(I i, J j) {
+    std::vector<long long> list {};
+    for (J x=i; x<=j; x++) list.push_back(S(x));
     return min(list);
 }
 
-template<typename N, typename A>
-std::vector<unsigned long> M(N n, A area) {
-    n = n-area;
-    if (n == 1000) {
-        ::j=501;
-        ::i=501;
-    }
-    else {
-        ::j=1;
-        ::i=1;
-    }
+template<typename S, typename E>
+std::vector<long long> M(S start, E end) {
+    std::vector<long long> results {};
 
-    std::vector<N> results = {};
-    for (::j; ::j<=n; ::j++) {
-        for (::i; ::i<=::j; ::i++) {
-            results.push_back(A(i, j));
+    for (E j=start+1; j<=end; j++) {
+        for (E i=1; i<=j; i++) {
+            results.push_back(A(i, j)); 
         }
     }
+
     return results;
-    /*unsigned long sum = 0;
-    for (auto it = results.begin(); it != results.end(); ++it) {
-        sum += *it;
-    }
-    return sum;*/
 }
 
 template<typename T>
-void Thread(T num) {
-    auto th1 = std::async(M, num, 1000);
-    //unsigned long th1v = th1.get();
-    auto th2 = std::async(M, num, 500);
-    //nsigned long th2v = th2.get();
+long long Thread(T num) {
+    auto a1 = std::async(M<T, T>, 0, num/2);
+    auto a2 = std::async(M<T, T>, num/2, num);
+
+    std::vector<long long> r = a1.get();
+    std::vector<long long> r2 = a2.get();
+    r.insert(r.end(), r2.begin(), r2.end());
+
+
+    long long sum = 0;
+    for (auto i=0; i<r.size(); i++) sum+=r[i];
+    return sum;
 }
 
 int main() {
+    auto num = 200;
+
     time_t tstart = time(NULL);
-    Thread(1000);
+    auto sum = Thread(num);
     time_t tend = time(NULL);
-    std::cout << tend-tstart << " second(s)." << std::endl;
+
+    std::cout << "M(" << num << ")= " << sum << "; " << tend-tstart << " second(s)." << std::endl;
     return 0;
 }
